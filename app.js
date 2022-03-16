9005b4ecc1f40e1337ad109f0121f64f4970a39c
chinita226 <@chinita226>
2022-03-16 10:59:53 +0100

    first init

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 1337;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log('App running on port '+ PORT);
+})
